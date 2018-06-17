c3aa3bbd6edc757cdda7ed590ce13ae5b774103c
Mona Lisa <email@example.com>
2018-06-17 16:29:04 +0800

    Commit

--- src/android/EntryActivity.java
+++ src/android/EntryActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import android.content.Context;
+
 public class EntryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
