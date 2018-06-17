726c4664541bbc276090e0d1f6fcd18600a97fdc
Mona Lisa <email@example.com>
2018-06-17 16:39:07 +0800

    Commit

--- src/android/EntryActivity.java
+++ src/android/EntryActivity.java
@@ -13,28 +13,32 @@ public class EntryActivity extends AppCompatActivity {
         // Check to see if this Activity is the root activity
         if (isTaskRoot()) {
 
-            Class mainActivity;
-            Context context = getApplicationContext();
-            String  packageName = context.getPackageName();
-            Intent  launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
-            String  className = launchIntent.getComponent().getClassName();
-
             try {
+
+               Class mainActivity;
+               Context context = getApplicationContext();
+               String  packageName = context.getPackageName();
+               Intent  launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+               String  className = launchIntent.getComponent().getClassName();
+
                 //loading the Main Activity to not import it in the plugin
                 mainActivity = Class.forName(className);
+
+                // This Activity is the only Activity, so
+                //  the app wasn't running. So start the app from the
+                //  beginning (redirect to MainActivity)
+                Intent mainIntent = getIntent(); // Copy the Intent used to launch me
+                // Launch the real root Activity (launch Intent)
+                mainIntent.setClass(this, mainActivity);
+                // I'm done now, so finish()
+                startActivity(mainIntent);
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
 
-            // This Activity is the only Activity, so
-            //  the app wasn't running. So start the app from the
-            //  beginning (redirect to MainActivity)
-            Intent mainIntent = getIntent(); // Copy the Intent used to launch me
-            // Launch the real root Activity (launch Intent)
-            mainIntent.setClass(this, mainActivity);
-            // I'm done now, so finish()
-            startActivity(mainIntent);
             finish();
+
         } else {
             // App was already running, so just finish, which will drop the user
             //  in to the activity that was at the top of the task stack
