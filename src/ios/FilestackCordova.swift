218e1c6a0bdbd78ec5d39a73ce94521964cdd70d
Mona Lisa <email@example.com>
2018-06-21 18:57:33 +0800

    Commit

---
+++ src/ios/FilestackCordova.swift
@@ -0,0 +1,44 @@
+@objc(FilestackCordova) class FilestackCordova : CDVPlugin {
+
+  @objc(openFilePicker:)
+  func openFilePicker(command: CDVInvokedUrlCommand) {
+    var pluginResult = CDVPluginResult(
+      status: CDVCommandStatus_ERROR
+    )
+
+    let msg = command.arguments[0] as? String ?? ""
+
+    if msg.characters.count > 0 {
+      let toastController: UIAlertController =
+        UIAlertController(
+          title: "",
+          message: msg,
+          preferredStyle: .alert
+        )
+
+      self.viewController?.present(
+        toastController,
+        animated: true,
+        completion: nil
+      )
+
+      DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+        toastController.dismiss(
+            animated: true,
+            completion: nil
+        )
+      }
+
+      pluginResult = CDVPluginResult(
+        status: CDVCommandStatus_OK,
+        messageAs: msg
+      )
+    }
+
+    self.commandDelegate!.send(
+      pluginResult,
+      callbackId: command.callbackId
+    )
+  }
+
+}
