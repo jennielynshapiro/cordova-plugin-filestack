5bd088b8711a1023779824561e56e60177bdd88f
Mona Lisa <email@example.com>
2018-06-23 18:02:39 +0800

    Commit

--- src/ios/FilestackCordova.swift
+++ src/ios/FilestackCordova.swift
@@ -4,64 +4,64 @@ import FilestackSDK
 @objc(FilestackCordova)
 class FilestackCordova : CDVPlugin {
 
-  @objc(openFilePicker:)
-  func openFilePicker(command: CDVInvokedUrlCommand) {
-    
-    var pluginResult = CDVPluginResult(
-      status: CDVCommandStatus_ERROR
-    )
+    var currentCallbackId:String = ""
 
-    let msg = command.arguments[0] as? String ?? ""
+    @objc(openFilePicker:)
+    func openFilePicker(command: CDVInvokedUrlCommand) {
 
-   if !msg.isEmpty {
+        let apiKey = command.arguments[0] as? String ?? ""
 
-        self.presentPicker(filestackAPIKey: msg);
+        if !apiKey.isEmpty {
 
-      pluginResult = CDVPluginResult(
-        status: CDVCommandStatus_OK,
-        messageAs: msg
-      )
+            self.currentCallbackId = command.callbackId
+
+            self.presentPicker(filestackAPIKey: apiKey);
+
+            let pluginResult = CDVPluginResult(status:CDVCommandStatus_NO_RESULT)
+            pluginResult?.setKeepCallbackAs(true)
+            self.commandDelegate!.send(
+                pluginResult,
+                callbackId: command.callbackId
+            )
+
+        } else {
+
+        }
     }
 
-    self.commandDelegate!.send(
-      pluginResult,
-      callbackId: command.callbackId
-    )
-  }
-    
     func presentPicker(filestackAPIKey: String) {
-        
-        self.commandDelegate!.run(inBackground: {
-            let config = Filestack.Config()
 
-            config.appURLScheme = "meldtables"
+        //self.commandDelegate!.run(inBackground: {
+        let config = Filestack.Config()
 
-            config.videoQuality = .typeHigh
+        config.appURLScheme = "meldtables"
 
-            if #available(iOS 11.0, *) {
-                config.imageURLExportPreset = .current
-                //config.videoExportPreset = AVAssetExportPresetHEVCHighestQuality
-            }
+        config.videoQuality = .typeHigh
+
+        if #available(iOS 11.0, *) {
+            config.imageURLExportPreset = .current
+            //config.videoExportPreset = AVAssetExportPresetHEVCHighestQuality
+        }
 
 
-            config.availableLocalSources = LocalSource.all()
+        config.availableLocalSources = LocalSource.all()
 
-            config.availableCloudSources = CloudSource.all()
+        config.availableCloudSources = CloudSource.all()
 
-            config.documentPickerAllowedUTIs = ["public.item"]
+        config.documentPickerAllowedUTIs = ["public.item"]
 
-            let client = Filestack.Client(apiKey: filestackAPIKey, config: config)
+        let client = Filestack.Client(apiKey: filestackAPIKey, config: config)
 
-            let storeOptions = StorageOptions(location: .s3)
+        let storeOptions = StorageOptions(location: .s3)
 
-            let picker = client.picker(storeOptions: storeOptions)
+        let picker = client.picker(storeOptions: storeOptions)
 
-            // Optional. Set the picker's delegate.
-            picker.pickerDelegate = self
+        // Optional. Set the picker's delegate.
+        picker.pickerDelegate = self
 
-            // Finally, present the picker on the screen.
-            self.viewController?.present(picker, animated: true)
-        })
+        // Finally, present the picker on the screen.
+        self.viewController?.present(picker, animated: true)
+        //})
     }
 
 }
@@ -69,10 +69,21 @@ class FilestackCordova : CDVPlugin {
 extension FilestackCordova: PickerNavigationControllerDelegate {
 
     func pickerUploadedFiles(picker: PickerNavigationController, responses: [NetworkJSONResponse]) {
+
         for response in responses {
             if let contents = response.json {
                 // Our local file was stored into the destination location.
                 print("Uploaded file response: \(contents)")
+
+                let result:[String:Any] = ["file":contents, "complete":false];
+
+                let pluginResult = CDVPluginResult(status:CDVCommandStatus_OK, messageAs:result)
+                pluginResult?.setKeepCallbackAs(false)
+                self.commandDelegate!.send(
+                    pluginResult,
+                    callbackId: self.currentCallbackId
+                )
+
             } else if let error = response.error {
                 // The upload operation failed.
                 print("Error uploading file: \(error)")
@@ -80,13 +91,21 @@ extension FilestackCordova: PickerNavigationControllerDelegate {
         }
     }
 
-
-
     func pickerStoredFile(picker: PickerNavigationController, response: StoreResponse) {
 
         if let contents = response.contents {
             // Our cloud file was stored into the destination location.
             print("Stored file response: \(contents)")
+
+            let result:[String:Any] = ["file":contents, "complete":false];
+
+            let pluginResult = CDVPluginResult(status:CDVCommandStatus_OK, messageAs:result)
+            pluginResult?.setKeepCallbackAs(false)
+            self.commandDelegate!.send(
+                pluginResult,
+                callbackId: self.currentCallbackId
+            )
+
         } else if let error = response.error {
             // The store operation failed.
             print("Error storing file: \(error)")
